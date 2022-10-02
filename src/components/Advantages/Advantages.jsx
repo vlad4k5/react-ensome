e556a826876cd7e4933140e336f420b08c6ba158
vlad4k5 <armia1823@gmail.com>
2022-10-02 21:25:33 +0300

    add Advantages component and some mixins for headlines and paragraphes

---
+++ src/components/Advantages/Advantages.jsx
@@ -0,0 +1,43 @@
+import s from './Advantages.module.scss'
+import brainCircuit from '../../assets/img/brainCircuit.svg'
+import arrowTrendingLines from '../../assets/img/arrowTrendingLines.svg'
+import key from '../../assets/img/key.svg'
+
+const Advantages = () => {
+  return (
+    <div className={s.advantagesContainer}>
+      <div className={s.advantagesContent}>
+        <h2>Why our clients chose Ensome?</h2>
+        <p>Doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+        
+        <div className={s.advantagesList}>
+          <div className={s.advantageItem}>
+            <div className={s.icon}>
+              <img src={brainCircuit} alt='Brain Circuit'/>
+            </div>
+            <h3>Machine learning</h3>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+          </div>
+
+          <div className={s.advantageItem}>
+            <div className={s.icon}>
+              <img src={arrowTrendingLines} alt='Trending Arrow'/>
+            </div>
+            <h3>Embed analytics</h3>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+          </div>
+
+          <div className={s.advantageItem}>
+            <div className={s.icon}>
+              <img src={key} alt='Key'/>
+            </div>
+            <h3>Access control</h3>
+            <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Advantages
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Advantages from '../../components/Advantages/Advantages';
 import Benefits from '../../components/Benefits/Benefits';
 
 const Home = () => {
   return (
     <>
       <Benefits />
+      <Advantages />
     </>
   );
 };
